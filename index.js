76a575875e9a43c7705a65c4f45f4b946b489896
Jelivian18 <barthen.mawu@gmail.com>
2024-07-15 23:35:48 +0800

    INCOME AND EXPENSE

---
+++ index.js
@@ -0,0 +1,14 @@
+const Note = require("./Note");
+// let note = new Note();
+// note.addIncome("Gaji Bulan", 5000);
+// note.addExpense("Bayar Listrik", 5000);
+// note.listIncome();
+// note.listExpense();
+// note.balance();
+// console.log(note);
+
+// Note.listIncome();
+// Note.listExpense();
+// Note.addIncome("Uang Masuk", 3000);
+// Note.addExpense("Bayar Tagihan", 500);
+Note.balance();
